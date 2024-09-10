a987ee6d51570920815a49b20d800107245cd974
cheasomnanglol12 <mybrolol096228@gmail.com>
2024-09-10 16:25:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------#
 # Configuration
 # ---------------------------------------------#
-# Recheck time in seconds to recheck all accounts (60 seconds = 1 minute and 0 means no recheck)
+# Recheck time in seconds to check all accounts again (60 seconds = 1 minute and 0 means no recheck)
 AccountsRecheckTime = 300
 
 # Adds a random delay to the AccountsRecheckTime interval to make it more unpredictable and less detectable.
@@ -12,11 +12,11 @@ MaxRandomDelay = 120
 # Accounts will be checked in the order they are listed
 AccountList = [
     {
-        "account_name": "cidnha",  # A custom name for the account (not important, just for logs)
-        "Authorization": "Bearer 1725952799827CoiYE5NKNEDvedEVQhWdT88DouC0dicw08Q0HP95dMHqqwmFGpS9BvIb9wEFqmAl7149859105",  # Account 1 Bearer token
-        "UserAgent": "Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",  # Provided UserAgent
-        "Proxy": {
-            "http": "http://113.195.224.222:9999",  # Updated proxy for Account 1
+        "account_name": "cid",  # A custom name for the account (not important, just for logs)
+        "Authorization": "Bearer 1725952799827CoiYE5NKNEDvedEVQhWdT88DouC0dicw08Q0HP95dMHqqwmFGpS9BvIb9wEFqmAl7149859105",  # To get the token, refer to the README.md file
+        "UserAgent": "Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",  # Refer to the README.md file to obtain a user agent
+        "Proxy": {           
+            "http": "http://113.195.224.222:9999"
         },
         "config": {
             "auto_tap": True,
@@ -24,10 +24,7 @@ AccountList = [
             "auto_get_daily_cipher": True,
             "auto_get_daily_task": True,
             "auto_get_task": True,
-            "auto_finish_mini_game": False,
-            "auto_claim_daily_combo": False,
-            "auto_daily_combo_enable": True,
-            "auto_daily_combo_max_price": 5_000_000,
+            "auto_finish_mini_game": True,
             "auto_playground_games": True,
             "auto_upgrade": True,
             "auto_upgrade_start": 2_000_000,
@@ -38,14 +35,14 @@ AccountList = [
             "show_num_buy_options": 0,
             "max_promo_games_per_round": 3,
         },
-        "telegram_chat_id": "7149859105",  # First Telegram chat ID for Account 1
+        "telegram_chat_id": "7149859105",
     },
     {
-        "account_name": "hengoszin",  # Second account configuration
-        "Authorization": "Bearer 172595298842289yMxIWMB3UE6AMouWE3123DDI7pQ7B456tBzFYEAFrZpsCiJCYD7ithjcIJb4Wi5001451526",  # Account 2 Bearer token
-        "UserAgent": "Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",  # Provided UserAgent
+        "account_name": "Accountn",
+        "Authorization": "Bearer 172595298842289yMxIWMB3UE6AMouWE3123DDI7pQ7B456tBzFYEAFrZpsCiJCYD7ithjcIJb4Wi5001451526",
+        "UserAgent": "Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",
         "Proxy": {
-            "http": "http://149.129.226.9:8080",  # Updated proxy for Account 2
+            "http": "http://149.129.226.9:8080"
         },
         "config": {
             "auto_tap": True,
@@ -53,10 +50,7 @@ AccountList = [
             "auto_get_daily_cipher": True,
             "auto_get_daily_task": True,
             "auto_get_task": True,
-            "auto_finish_mini_game": False,
-            "auto_claim_daily_combo": False,
-            "auto_daily_combo_enable": True,
-            "auto_daily_combo_max_price": 5_000_000,
+            "auto_finish_mini_game": True,
             "auto_playground_games": True,
             "auto_upgrade": True,
             "auto_upgrade_start": 2_000_000,
@@ -67,14 +61,14 @@ AccountList = [
             "show_num_buy_options": 0,
             "max_promo_games_per_round": 3,
         },
-        "telegram_chat_id": "1439771387",  # Second Telegram chat ID for Account 2
+        "telegram_chat_id": "1439771387",
     },
     {
-        "account_name": "Account 3",  # Third account configuration
-        "Authorization": "Bearer 1725953159405kGu1IiqUc60wj7AQUfFoG5umoUBBJYp4knlMJMkgMDP5vVlUFaBOncnR2ZBW0Anj5129273722",  # Account 3 Bearer token
-        "UserAgent": "Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",  # Provided UserAgent
+        "account_name": "mine",
+        "Authorization": "Bearer 1725953159405kGu1IiqUc60wj7AQUfFoG5umoUBBJYp4knlMJMkgMDP5vVlUFaBOncnR2ZBW0Anj5129273722",
+        "UserAgent": "Mozilla/5.0 (Linux; Android 14; K) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/127.0.6533.103 Mobile Safari/537.36",
         "Proxy": {
-            "http": "http://120.42.224.254:21212",  # Updated proxy for Account 3
+            "http": "http://120.42.224.254:21212"
         },
         "config": {
             "auto_tap": True,
@@ -82,10 +76,7 @@ AccountList = [
             "auto_get_daily_cipher": True,
             "auto_get_daily_task": True,
             "auto_get_task": True,
-            "auto_finish_mini_game": False,
-            "auto_claim_daily_combo": False,
-            "auto_daily_combo_enable": True,
-            "auto_daily_combo_max_price": 5_000_000,
+            "auto_finish_mini_game": True,
             "auto_playground_games": True,
             "auto_upgrade": True,
             "auto_upgrade_start": 2_000_000,
@@ -96,8 +87,8 @@ AccountList = [
             "show_num_buy_options": 0,
             "max_promo_games_per_round": 3,
         },
-        "telegram_chat_id": "1439771387",  # First Telegram chat ID for Account 3
-    },
+        "telegram_chat_id": "1439771387",
+    }
 ]
 
 # ---------------------------------------------#
@@ -109,9 +100,8 @@ AccountList = [
 # If you don't want to use this feature, set "is_active" to False and leave "bot_token" and "uid" fields empty.
 # This feature is optional, and you can disable it by setting "is_active" to False.
 telegramBotLogging = {
-    "is_active": True,  # Set to True to activate Telegram logging
-    "bot_token": "7069215582:AAEXyTWTu4KdhblkgFhG5_IbbtTkZjSEbTo",  # Provided bot token
-    # Configure the what you want to receive logs from the bot
+    "is_active": True,
+    "bot_token": "7069215582:AAEXyTWTu4KdhblkgFhG5_IbbtTkZjSEbTo",
     "messages": {
         "general_info": True,
         "account_info": True,
